05fee8cb6465b538c2768a97992947fcdce50c4c
Cal Farrell Woods <153258375+Cal-Woods@users.noreply.github.com>
2025-01-20 23:03:55 +0000

    Uploaded "PropTypes" folder contents.

---
+++ public/PropTypes/types.tsx
@@ -0,0 +1,3 @@
+//define type for type safe props
+export type blueMessageProps = {title:string, message:string};
+export type cardProps = {buttonLink:string, buttonText:string, desc:string, title:string, message:string, imgLink:string};
