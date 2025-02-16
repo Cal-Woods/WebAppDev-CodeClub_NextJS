232f53c5c5bbd1bdcc19a3e6860a39e76dfe91c1
Cal Woods <D00270955@student.dkit.ie>
2025-02-16 21:11:46 +0000

    Updated 'app/DB-Interactions/Fetch-Tables/data.ts'.

---
+++ app/DB-Interactions/Fetch-Tables/data.ts
@@ -0,0 +1,16 @@
+import { cardProps } from '@/public/PropTypes/types';
+import {sql} from '@vercel/postgres';
+
+export async function fetchHomeCardData() {
+    //Try to fetch data from the db
+    try {
+        const data = await sql<cardProps>`SELECT * FROM card_data WHERE id ILIKE 'HC_'`; 
+
+        return data.rows;
+    }
+    //Catch a possible error
+    catch(error) {
+        console.error('Database Error:', error);
+        throw new Error("Could not fetch card data!");
+    }
+}
