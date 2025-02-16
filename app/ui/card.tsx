18c97c5c8f13846290936a69aedcbe7bcd9c7da7
Cal Woods <D00270955@student.dkit.ie>
2025-02-16 21:06:03 +0000

    Updated Card component.

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -11,10 +11,10 @@ export default function Card(data:cardProps) {
 
     return (
         <div className={`w-[300px] h-[350px] block bg-blue-800  mb-[12px] border-2 border-black rounded-lg text-sm m-auto sm:bg-yellow-500 sm:text-lime-500 md:m-0 md:text-base md:bg-red-600 md:mb-0 md:mr-[12px] lg:bg-transparent lg:text-black`}>
-            <Image alt={data.id+" "+data.description} src={data.imgLink} width={300} height={64}/>
+            <Image alt={data.id+" "+data.description} src={data.imglink} width={300} height={64}/>
             <h2 className="font-extrabold">{data.title}</h2>
             <div className="h-[95px] overflow-auto border-2 border-black"><p className="font-bold">{data.message}</p></div>
-            <Link className={clsx("w-[100%] relative -bottom-[35px] bg-slate-400 border-2 border-black hover:bg-blue-600 active:bg-white", path!="/" ? "hidden" : "block")} href={data.buttonLink}>{data.buttonText}</Link>
+            <Link className={clsx("w-[100%] relative -bottom-[35px] bg-slate-400 border-2 border-black hover:bg-blue-600 active:bg-white", path!="/" ? "hidden" : "block")} href={data.buttonlink}>{data.buttontext}</Link>
         </div>
     );
 }
