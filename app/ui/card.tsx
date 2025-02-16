52a3b0984e3235e196f9822ce18a35d2492b69c6
Cal Woods <D00270955@student.dkit.ie>
2025-02-16 18:41:31 +0000

    Updated "app/ui/card.tsx" by changing multiple props into one prop for db data.

--- app/ui/card.tsx
+++ app/ui/card.tsx
@@ -6,16 +6,16 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-export default function Card({buttonLink, buttonText, description, message, imgLink, title}:cardProps) {
+export default function Card(data:cardProps) {
     const path = usePathname();
 
     return (
         <div className={`w-[300px] h-[350px] block bg-blue-800  mb-[12px] border-2 border-black rounded-lg text-sm m-auto sm:bg-yellow-500 sm:text-lime-500 md:m-0 md:text-base md:bg-red-600 md:mb-0 md:mr-[12px] lg:bg-transparent lg:text-black`}>
-            <Image alt={description} src={imgLink} width={300} height={64}/>
-            <h2 className="font-extrabold">{title}</h2>
-            <div className="h-[95px] overflow-auto border-2 border-black"><p className="font-bold">{message}</p></div>
-            <Link className={clsx("w-[100%] relative -bottom-[35px] bg-slate-400 border-2 border-black hover:bg-blue-600 active:bg-white", path!="/" ? "hidden" : "block")} href={buttonLink}>{buttonText}</Link>
+            <Image alt={data.id+" "+data.description} src={data.imgLink} width={300} height={64}/>
+            <h2 className="font-extrabold">{data.title}</h2>
+            <div className="h-[95px] overflow-auto border-2 border-black"><p className="font-bold">{data.message}</p></div>
+            <Link className={clsx("w-[100%] relative -bottom-[35px] bg-slate-400 border-2 border-black hover:bg-blue-600 active:bg-white", path!="/" ? "hidden" : "block")} href={data.buttonLink}>{data.buttonText}</Link>
         </div>
     );
-    }
+}
 
