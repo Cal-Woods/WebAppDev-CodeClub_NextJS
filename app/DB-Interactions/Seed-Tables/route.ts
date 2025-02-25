d91994bef622bca9373bc464b6b745b36d5413d4
Cal Woods <D00270955@student.dkit.ie>
2025-02-25 13:17:57 +0000

    Added 'seedMenuDataTable()' function to 'GET()' functionality.

--- app/DB-Interactions/Seed-Tables/route.ts
+++ app/DB-Interactions/Seed-Tables/route.ts
@@ -1,10 +1,14 @@
 
-import { cards } from "@/public/DB-Data/card-inserts";
+import { cards, menuData } from "@/public/DB-Data/card-inserts";
 import { db } from "@vercel/postgres";
 
 //Declare constant variable for db connection
 const client = await db.connect();
 
+/**
+ * Creates & seeds the 'card_data' table in connected database.
+ * @returns Promise result of inserting cards
+ */
 async function seedCardDataTable() {
     //Use 'client.sql' to query database: Create table for homecarddata with columns matching data type 'Card', Store 
     await client.sql`
@@ -32,13 +36,42 @@ async function seedCardDataTable() {
     return insertedCards;
 }
 
+/**
+ * Creates & seeds 'menu_data' table in database.
+ * @returns Promise results of inserted menu entries
+ */
+async function seedMenuDataTable() {
+    //Use 'client.sql' to query database: Create table for menu data with columns matching data type 'menuData'
+    await client.sql`
+    CREATE TABLE IF NOT EXISTS menu_data (
+    id VARCHAR(255) NOT NULL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    link VARCHAR(255) NOT NULL
+    );`;
+
+    //Storing an array with menu data in 'insertedMenuEntries'
+    const insertedMenuEntries = await Promise.all(
+        //'map()' function iterates over given data and does something for each one.
+        // 
+        menuData.map((entry) => client.sql`
+            INSERT INTO menu_data VALUES (${entry.id}, ${entry.title}, ${entry.link})
+            ON CONFLICT (link) DO NOTHING;
+    `,
+        ),
+    );
+    
+    return insertedMenuEntries;
+}
+
 export async function GET() {
     //Initiate try-catch statement for interacting with database & catching errors
     try {
         //Run sql 'BEGIN' query
         await client.sql`BEGIN`;
-        //Call function 'createTables'
+        //Call function 'seedCardDataTable'
         await seedCardDataTable();
+        //Call function 'seedMenuDataTable'
+        await seedMenuDataTable();
         //Run sql 'COMMIT' query
         await client.sql`COMMIT`;
 
