f86c7cfcd96576ca65f1f5bc480db846d91f8123
Cal Woods <D00270955@student.dkit.ie>
2025-02-25 12:58:03 +0000

    Added Type 'menu' for getting menu entries from database.

--- public/PropTypes/types.tsx
+++ public/PropTypes/types.tsx
@@ -2,4 +2,4 @@
 export type blueMessageProps = {title:string, message:string};
 export type cardProps = {buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
 export type cardData = {id:string, buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
-export type menuData = {id:string, title:string, link:string};
+export type menu = {id:string, title:string, link:string};
