d9876f8610b612aac1b3092d7ee043f809c49706
Cal Woods <D00270955@student.dkit.ie>
2025-02-26 20:30:22 +0000

    Updated both nav menus to use map() to create menu entries based on given data.

--- app/ui/adaptiveMenu.tsx
+++ app/ui/adaptiveMenu.tsx
@@ -28,11 +28,9 @@ export function CCSmallMenu({data}:{data:menu[]}) {
                 "w-[50%] float-right z-10 absolute top-10 right-[1%] clear-right transition-opacity bg-cyan-500 mb-[18px] text-center m-auto pt-[8px]",
 
                 {'block opacity-100':mobileMenu, 'opacity-0 hidden':!mobileMenu})}>
-
-                <Link href={data[0].link} className="w-[100%] border-b-2 pb-4 relative border-white hover:bg-slate-300 active:bg-slate-200 block" onClick={clickLink}>{data[0].title}</Link>
-                <Link href={data[1].link} className="w-[100%] border-b-2 pb-4 relative border-white hover:bg-slate-300 active:bg-slate-200 block" onClick={clickLink}>{data[1].title}</Link>
-                <Link href={data[2].link} className="w-[100%] border-b-2 pb-4 relative border-white hover:bg-slate-300 active:bg-slate-200 block" onClick={clickLink}>{data[2].title}</Link>
-                <Link href={data[3].link} className="w-[100%] border-b-2 pb-4 relative border-white hover:bg-slate-300 active:bg-slate-200 block" onClick={clickLink}>{data[3].title}</Link>
+                
+                {data.map((entry) => {return <Link key={entry.id} href={entry.link} className="w-[100%] border-b-2 pb-4 relative border-white hover:bg-slate-300 active:bg-slate-200 block" onClick={clickLink}>{entry.title}</Link>
+})}
             </nav>
         </div>
     );
@@ -42,10 +40,7 @@ export function CCLgMenu({data}:{data:menu[]}) {
     return (
     <div className="w-full h-[35px] hidden text-base md:block md:mb-[25px] md:text-red-600 md:bg-white md:rounded-md md:border-2 overflow-auto md:border-black md:clear-left lg:bg-transparent lg:border-none lg:text-black lg:h-fit">
         <nav id="Large-data" className="w-full md:clear-left lg:block lg:border-t-2 lg:border-t-black lg:p-0">
-            <Link href={data[0].link} className="w-full md:border-x-2 md:px-[35px] md:mr-[100px] lg:border-b-2 lg:text-center lg:border-x-0 hover:bg-cyan-400 active:bg-green-300 lg:border-b-black lg:pb-[8px] inline lg:block"><button>{data[0].title}</button></Link>
-            <Link href={data[1].link} className="w-full md:px-[35px] md:mr-[100px] lg:border-b-2 md:border-x-2 lg:border-x-0 lg:border-b-black lg:text-center hover:bg-cyan-400 active:bg-orange-400 lg:pb-[8px] line lg:block"><button>{data[1].title}</button></Link>
-            <Link href={data[2].link} className="w-full md:px-[34px] md:mr-[100px] lg:border-b-2 md:border-x-2 lg:border-x-0 lg:border-b-black lg:text-center lg:p-0 hover:bg-cyan-400 active:bg-cyan-200 lg:pb-[8px] inline lg:block"><button>{data[2].title}</button></Link>
-            <Link href={data[3].link} className="w-full md:border-x-2 md:px-[34px] lg:border-x-0 hover:bg-cyan-400 lg:text-center lg:p-0 lg:pb-[8px] active:bg-red-500 inline lg:block lg:border-b-2 lg:border-b-black"><button>{data[3].title}</button></Link>
+            {data.map((entry) => {return <Link key ={entry.id} href={entry.link} className="w-full md:border-x-2 md:px-[35px] md:mr-[100px] lg:border-b-2 lg:text-center lg:border-x-0 hover:bg-cyan-400 active:bg-green-300 lg:border-b-black lg:pb-[8px] inline lg:block"><button>{entry.title}</button></Link>})}
         </nav>
     </div>
     );
