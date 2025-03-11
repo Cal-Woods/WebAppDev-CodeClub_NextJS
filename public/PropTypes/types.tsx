5cd1dfc0a48b04f98f1ca7fb35ab6a2e550edeb2
Cal Woods <D00270955@student.dkit.ie>
2025-03-11 10:42:04 +0000

    Updated cardData type by adding pos : number field for database inserts.
    Each piece of cards data now holds a pos value.
    
    pos set randomly by me, not in code, to test ordering of Card components.

--- public/PropTypes/types.tsx
+++ public/PropTypes/types.tsx
@@ -1,5 +1,5 @@
 //define type for type safe props
 export type blueMessageProps = {title:string, message:string};
 export type cardProps = {buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
-export type cardData = {id:string, buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
+export type cardData = {id:string, pos:number, buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
 export type menu = {id:string, title:string, link:string};
