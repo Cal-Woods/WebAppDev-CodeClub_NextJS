68f3cac54167644e6bba7f8e1c5c565d1c133d08
Cal Woods <D00270955@student.dkit.ie>
2025-03-25 09:35:56 +0000

    Update: Added placeholder user credentials.
    One admin and two regular users.
    
    Other user info: name, email, etc, wil be stored in another file.

--- app/DB-Interactions/(User-DB)/placeholder-data.ts
+++ app/DB-Interactions/(User-DB)/placeholder-data.ts
@@ -1,5 +1,17 @@
-export const fakeUserData = [
+export const fakeUserCreds = [
     {
-        
+        id:1,
+        username:"Admin",
+        password:"password"
+    },
+    {
+        id:2,
+        username:"Cal",
+        password:"A_pA$54raSE_W0rtH_R3Membering"
+    },
+    {
+        id:3,
+        username:"D00541789",
+        password:"Cam3$Le0N_889"
     }
 ];
