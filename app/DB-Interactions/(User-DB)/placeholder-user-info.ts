6f9c28a3f7034117446f132462ba821e1f2beec2
Cal Woods <D00270955@student.dkit.ie>
2025-03-25 12:55:01 +0000

    Update: Removed entry from userInfo array and changed entry.
    Removed entry wth id 3.
    
    Changed entry with id 2, now fName is Peter, lName is Gosling and email is gosl1ng@lecturer.com, was Cal, Woods, calfarrell7785@gmail.com.

---
+++ app/DB-Interactions/(User-DB)/placeholder-user-info.ts
@@ -0,0 +1,16 @@
+export const userInfo = [
+    {
+        userId:1,
+        fName:"Administrator",
+        lName:"Woods",
+        email:"calwoods7785@gmail.com",
+        accountType:"admin"
+    },
+    {
+        userId:2,
+        fName:"Peter",
+        lName:"Gosling",
+        email:"gosl1ngp@lecturer.com",
+        accountType:"coder"
+    }
+];
