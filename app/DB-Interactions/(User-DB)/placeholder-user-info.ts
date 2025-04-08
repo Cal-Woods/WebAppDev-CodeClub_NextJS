0b676b853d51889742d9520f91d49116c1969bbd
Cal Woods <D00270955@student.dkit.ie>
2025-04-08 10:24:36 +0100

    Update: Added date of birth(dob) field to each entry.

--- app/DB-Interactions/(User-DB)/placeholder-user-info.ts
+++ app/DB-Interactions/(User-DB)/placeholder-user-info.ts
@@ -5,6 +5,7 @@ export const userInfo = [
         password:"password",
         fName:"Administrator",
         lName:"Woods",
+        dob: "1999-07-06",
         email:"calwoods7785@gmail.com",
         accountType:"admin",
         interest:"Python"
@@ -15,6 +16,7 @@ export const userInfo = [
         password:"A_pA$54raSE_W0rtH_R3Membering",
         fName:"Peter",
         lName:"Gosling",
+        dob: "2000-08-30",
         email:"gosl1ngp@lecturer.com",
         accountType:"coder",
         interest:"Scratch"
