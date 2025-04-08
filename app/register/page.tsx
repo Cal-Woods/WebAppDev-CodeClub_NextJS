b3b8ac6bfa69266610db5aeec9be93021c946e80
Cal Woods <D00270955@student.dkit.ie>
2025-04-08 10:38:15 +0100

    Update: Divided form into fieldsets to section-off data.
    Added submit button to send data.
    
    Not fully configured to send data yet.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -3,35 +3,46 @@ import { BlueMessage } from "../ui/blueMessage"
 export default function Page() {
     return (
       <div id="registration">
-      <BlueMessage title="Register" message="Sign up for an account to be able to book yourself into upcoming events."></BlueMessage>
-      <div id="outer" className="w-[400px] md:w-[750px] lg:w-[1000px] h-fit border-black border-x-2 border-y-2 md:mt-4 m-auto clear-both bg-white align-middle text-black">
-        <div id="inner" className="w-full m-auto text-center">
-          <form id="register" aria-label="Sign-up form.">
-            <legend aria-label="Personal Details" className="text-lg font-semibold underline mt-2">Personal Details</legend>
+        <BlueMessage title="Register" message="Sign up for an account to be able to book yourself into upcoming events."></BlueMessage>
 
-            <label htmlFor="firstName">First Name</label> <br />
-            <input type="text" name="firstName" id="firstName" placeholder="Enter first name:" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
+        <div id="outer" className="w-[400px] md:w-[750px] lg:w-[1000px] h-fit border-black border-x-2 border-y-2 md:mt-4 m-auto clear-both bg-white align-middle text-black">
+          <div id="inner" className="w-full m-auto text-center">
+            <form id="register" aria-label="Sign-up form.">
+              <fieldset className="border-2 border-dotted border-red-600">
+                <legend aria-label="Personal Details" className="text-lg font-semibold underline mt-2 m-auto">Personal Details</legend>
 
-            <label htmlFor="lastName" className="">Last Name</label> <br />
-            <input type="text" name="lastName" id="lastName" placeholder="Enter last name" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
+                <label htmlFor="firstName">First Name</label> <br />
+                <input type="text" name="firstName" id="firstName" placeholder="Enter first name:" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
 
-            <label htmlFor="date">Date of birth</label> <br />
-            <input type="date" name="date" id="date" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
+                <label htmlFor="lastName" className="">Last Name</label> <br />
+                <input type="text" name="lastName" id="lastName" placeholder="Enter last name" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
 
-            <label htmlFor="email">Email</label> <br />
-            <input type="email" id="email" placeholder="Enter email address" className="mt-2 w-[90%] border-2 border-black drop-shadow-sm"></input> <br />
+                <label htmlFor="date">Date of birth</label> <br />
+                <input type="date" name="date" id="date" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
 
-            <select aria-label="select area of interest" name="interest" id="interest" defaultValue="default" className="w-[90%] text-center border-2 drop-shadow-md border-black bg-blue-700">
-              <option value="default">Area of interest</option>
-              <option value="scratch">Scratch</option>
-              <option value="python">Python</option>
-              <option value="web">Web</option>
-            </select>
+                <label htmlFor="email">Email</label> <br />
+                <input type="email" id="email" placeholder="Enter email address" className="mt-2 w-[90%] border-2 border-black drop-shadow-sm"></input> <br />
 
+                <select aria-label="select area of interest" name="interest" id="interest" defaultValue="default" className="w-[90%] text-center border-2 drop-shadow-md border-black bg-blue-700">
+                  <option value="default">Area of interest</option>
+                  <option value="scratch">Scratch</option>
+                  <option value="python">Python</option>
+                  <option value="web">Web</option>
+                </select> <br />
+              </fieldset>
 
-          </form>
+              <fieldset className="border-2 border-dotted border-red-600">
+                <legend aria-label="username and password" className="text-lg font-semibold underline mt-2 m-auto">Username & Password</legend>
+                <label htmlFor="user">Username</label> <br />
+                <input type="text" name="user" id="user" className="w-[90%] mt-2 border-black border-2"></input> <br />
+                <label htmlFor="pass">Password</label> <br />
+                <input type="password" name="pass" id="pass" autoComplete="false" className="w-[90%] mt-2 border-black border-2"></input> <br />
+              </fieldset>
+
+              <button name="reg_Submit" type="submit" className="w-[90%] border-2 border-black bg-slate-400 hover:bg-white active:bg-cyan-200 active:text-black">Submit details</button>
+            </form>
+          </div>
         </div>
-      </div>
       </div>
     );
 }
