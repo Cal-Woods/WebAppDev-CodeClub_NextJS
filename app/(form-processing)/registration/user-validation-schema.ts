ed0ba1370c5f1c54433ecb370e2e1ff826875e91
Cal Woods <D00270955@student.dkit.ie>
2025-04-14 12:24:10 +0100

    File: Added new file to /app/(form-processing)/registration for validating registration form with zod.
    A const called registrationSchema is exported from this file.
    
    The web app will use this schema for validating the registration form using the set rules in this schema object.

---
+++ app/(form-processing)/registration/user-validation-schema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+export const registrationSchema = z.object({
+    email: z.string().email({message: "Email may be missing an '@' or 'something.com', please enter a valid email address!"}),
+    password: z.string().min(8, {message: "The password must be at least 8 characters long!"})
+    .regex(/[a-zA-Z]/, {message: "Password must contain at least one letter!"})
+    .regex(/[0-9]/, {message: "Password must contain at least one number!"})
+    .regex(/[^a-zA-Z0-9]/, {message:"Password must contain at least one special character!"})
+    .trim()
+})
