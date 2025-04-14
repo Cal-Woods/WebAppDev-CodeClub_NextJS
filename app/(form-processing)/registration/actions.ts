36809234bf074c39cd75145f59765accf84baa26
Cal Woods <D00270955@student.dkit.ie>
2025-04-14 10:28:07 +0100

    Refactor: Replaced import with hard-coded const.
    Removed importing db connection string const client from Seed-Tables route to use a hard-coded const client for the db connection string.
    
    Made necessary due to ESLint build error Type error: Type 'OmitWithTag<typeof import("C:/Users/helpm/Documents/Web_Apps_Dev/NextJS/WebAppDev-CodeClub_NextJS/app/DB-Interactions/Seed-Tables/route"), "GET" | "HEAD" | "OPTIONS" | "POST" | "PUT" | ... 10 more ... | "maxDuration", "">' does not satisfy the constraint '{ [x: string]: never; }'.

--- app/(form-processing)/registration/actions.ts
+++ app/(form-processing)/registration/actions.ts
@@ -1,10 +1,15 @@
 'use server'
 
+import { db } from "@vercel/postgres"
 import bcrypt from "bcryptjs"
 import { permanentRedirect, RedirectType } from "next/navigation"
-import { client } from "@/app/DB-Interactions/Seed-Tables/route"
 
-export async function handleSignUp(formData: FormData) {
+//Store database connection string
+const client = await db.connect()
+
+export async function handleSignUp(formData: FormData):Promise<void> {
+    
+
     //Get all submitted data as string values
     const username = formData.get("user") as string
     const pass = formData.get("pass") as string
@@ -24,11 +29,11 @@ export async function handleSignUp(formData: FormData) {
     const hash = await bcrypt.hash(pass, await bcrypt.genSalt(13))
 
     //Query db and insert formData
-    client.sql`BEGIN`
-    client.sql`INSERT INTO user_info (username, password, first_name, last_name, birth_date, email, account_type, primary_interest) VALUES(
+    await client.sql`BEGIN`
+    await client.sql`INSERT INTO user_info (username, password, first_name, last_name, birth_date, email, account_type, primary_interest) VALUES(
         ${username}, ${hash}, ${fName}, ${lName}, ${dob}, ${email}, 'coder', ${interest}
     )`
-    client.sql`COMMIT`
+    await client.sql`COMMIT`
 
 
     return
