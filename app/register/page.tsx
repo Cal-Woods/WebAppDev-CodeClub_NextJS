d302d61d99543462243259826ef33d7e24212222
Cal Woods <D00270955@student.dkit.ie>
2025-04-14 09:53:07 +0100

    Refactor: Removed method from form & replaced with server action.
    Various name changes for form data.
    
    Server action called handleSignUp in /app/(form-processing)/registration/actions.ts.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,3 +1,4 @@
+import { handleSignUp } from "../(form-processing)/registration/actions";
 import { BlueMessage } from "../ui/blueMessage"
 
 export default function Page() {
@@ -7,7 +8,7 @@ export default function Page() {
 
         <div id="outer" className="w-[400px] md:w-[750px] lg:w-[1000px] h-fit border-black border-x-2 border-y-2 md:mt-4 m-auto clear-both bg-white align-middle text-black">
           <div id="inner" className="w-full m-auto text-center">
-            <form id="register" aria-label="Sign-up form" action="/registration" method="post">
+            <form id="register" aria-label="Sign-up form" action={handleSignUp}>
               <fieldset className="border-2 border-dotted border-red-600">
                 <legend aria-label="Personal Details" className="text-lg font-semibold underline mt-2 m-auto">Personal Details</legend>
 
@@ -18,10 +19,10 @@ export default function Page() {
                 <input type="text" name="lastName" id="lastName" placeholder="Enter last name" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
 
                 <label htmlFor="date">Date of birth</label> <br />
-                <input type="date" name="date" id="date" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
+                <input type="date" name="dob" id="date" className="w-[90%] mt-2 border-2 border-black drop-shadow-sm"/> <br /> <br />
 
                 <label htmlFor="email">Email</label> <br />
-                <input type="email" id="email" placeholder="Enter email address" className="mt-2 w-[90%] border-2 border-black drop-shadow-sm"></input> <br />
+                <input type="email" name="email" id="email" placeholder="Enter email address" className="mt-2 w-[90%] border-2 border-black drop-shadow-sm"></input> <br />
 
                 <select aria-label="select area of interest" name="interest" id="interest" defaultValue="default" className="w-[90%] text-center border-2 drop-shadow-md border-black bg-blue-700">
                   <option value="default">Area of interest</option>
