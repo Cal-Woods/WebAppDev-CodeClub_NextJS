9c1acd0a5fe946e49481a6cf505ffbb268cb7a32
Cal Woods <D00270955@student.dkit.ie>
2025-04-27 14:30:16 +0100

    Update: Added section for validating password & confirm password data on server-side.
    Using Zod package trim() function on confirmPassword field to remove un-needed elements.
    
    Added code that uses Zod superRefine() function on new Zod schema object.

--- app/(form-processing)/registration/user-validation-schema.ts
+++ app/(form-processing)/registration/user-validation-schema.ts
@@ -1,22 +1,34 @@
 import {z} from "zod"
 
+//Place rules on captured form data using z.object() from Zod package
 export const registrationSchema = z.object({
-    email: z.string().email({message: "Email may be missing an '@' or 'something.com', please enter a valid email address!"}),
+    email: z.string().email({message: "Email may be missing an '@' or 'something.com', please enter a valid email address!"}).trim(),
     password: z.string().min(8, {message: "The password must be at least 8 characters long!"})
     .regex(/[a-zA-Z]/, {message: "Password must contain at least one letter!"})
     .regex(/[0-9]/, {message: "Password must contain at least one number!"})
     .regex(/[^a-zA-Z0-9]/, {message:"Password must contain at least one special character!"})
-    .trim()
+    .trim(),
+    confirmPassword: z.string().trim(),
+}).superRefine((val, ctx) => {
+    if(val.password !== val.confirmPassword) {
+        ctx.addIssue({
+            code: z.ZodIssueCode.custom,
+            message: "Password fields do not match, they must match!",
+            path: ["confirmPassword"],
+        })
+    }
 })
 
 export type registerState = {
     form?: {
         email?: string,
         password?: string,
+        confirmPassword?: string
     }
 
     errors?:{
         email?: string[],
-        password?:string[]
+        password?:string[],
+        confirmPassword?:string[]
     }
 }
