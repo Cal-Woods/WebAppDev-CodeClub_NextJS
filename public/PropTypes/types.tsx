3b84e4459f7f8f5d87ff1f7594ab39b94ce1513e
Cal Woods <D00270955@student.dkit.ie>
2025-04-28 23:58:10 +0100

    Update: Created two new types for holding database user data.
    userVerifyData & userData.
    
    userVerifyData holds a username & password for initial login processing, userData holds the rest of the data for when user identity is confirmed.

--- public/PropTypes/types.tsx
+++ public/PropTypes/types.tsx
@@ -3,3 +3,5 @@ export type blueMessageProps = {title:string, message:string};
 export type cardProps = {buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
 export type cardData = {id:string, pos:number, buttonlink:string, buttontext:string, description:string, title:string, message:string, imglink:string};
 export type menu = {id:string, title:string, link:string};
+export type userData = {username:string, fName:string, lName:string, dob:Date, email:string, type:string, interest:string};
+export type userVerifyData = {username:string, password:string}
