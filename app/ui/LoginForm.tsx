702ceeb3ed31628dbc1a5ceb3856b0ea54962bcd
Cal Woods <D00270955@student.dkit.ie>
2025-05-01 10:30:38 +0100

    Update: Added use client directive to top of file and set form action to the useActionState() hook function for handling a form state returned by a server action.
    The import was a server function called handleLogin() which was used in an array with three variables: state, action, isPending, these are assigned by the useActionState() React hook function.
    
    Form will only show errors or messages if they are returned by the server action, ie, if they exist.

--- app/ui/LoginForm.tsx
+++ app/ui/LoginForm.tsx
@@ -1,21 +1,25 @@
-export default function LoginForm() {
+'use client'
 
+import { useActionState } from "react";
+import { handleLogin } from "../(form-processing)/registration/actions";
+
+export default function LoginForm() {
+    const [state, action, isPending] = useActionState(handleLogin, undefined)
 
     return(
-        <div id="login-form-outer" className="min-w-[450px] max-w-[650px] sm:min-w-[550px] sm:max-w-[800px] md:min-w-[900px] md:max-w-[1200px] lg:min-w-[1200px] h-fit m-auto ">
-            <div id="login-form-inner" className="w-[90%] h-[300px] m-auto text-center bg-green-400">
-                <form id="login-form" action="#">
-                    <fieldset className="border-white border-dotted border-2">
-                        <legend className="m-auto pt-8">Login</legend>
-                        <label htmlFor="userName" className="inline-block mt-8 text-center">Enter username</label> <br />
-                        <input type="text" name="username" id="userName" placeholder="Enter username" className="w-full border-2 border-black rounded-sm" required/>
+        <div id="login-form-outer" className="min-w-[450px] max-w-[650px] sm:min-w-[550px] sm:max-w-[800px] md:min-w-[900px] md:max-w-[1200px] lg:min-w-[1200px] m-auto border-white border-2 border-dotted">
+            <div id="login-form-inner" className="w-[90%] h-[300px] m-auto text-center bg-green-300">
+                <form id="login-form" action={action} className="h-fit text-black">
+                    <label htmlFor="userName" className="inline-block mt-8 text-center">Enter username</label> <br />
+                    <input type="text" name="username" id="userName" defaultValue={state?.username} placeholder="Enter username" className="w-full border-2 border-black rounded-sm" required/>
+                    {state?.error && <p className="mt-4 text-sm text-red-700">{state.error}</p>}
 
-                        <label htmlFor="passWord" className="inline-block mt-8 text-center">Enter password</label> <br />
-                        <input type="password" name="password" id="passWord" placeholder="Enter password" className="w-full border-2 border-black rounded-sm text-black" minLength={8} required/>
+                    <label htmlFor="passWord" className="inline-block mt-8 text-center">Enter password</label> <br />
+                    <input type="password" name="password" id="passWord" placeholder="Enter password" className="w-full border-2 border-black rounded-sm text-black" minLength={8} required/>
 
-                        <button type="submit" className="w-full mt-14 bg-slate-500 hover:bg-slate-400 active:bg-slate-200">Submit</button>
-                    </fieldset>
+                    <button type="submit" disabled={isPending} className="w-full mt-4 bg-slate-500 hover:bg-slate-400 active:bg-slate-200">Submit</button>
                 </form>
+                {state?.message && <p className="mt-4 text-sm text-white sm:text-green-600">{state.message}</p>}
             </div>
         </div>
     );
